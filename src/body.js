3e41dc3872b930e219845ec54482c169721d35a8
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-02-08 15:06:49 -0800

    add body to page

---
+++ src/body.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import print from './print-icon.png';
+import pizza from './choco-pizza.png';
+import { ingredientItems } from './IngredientList.js';
+
+export default class MyBody extends React.Component {
+    render() {
+        const ingList = ingredientItems.map(ing => <div>
+            <p>{ing.amount} {ing.name}</p>
+        </div>
+        );
+
+        return (
+            <div>
+                <div className='left'>
+                    <h1>Chocolate Pizza</h1>
+                    <h4>POSTED ON 15 DEC 2013/DESSERTS</h4>
+                </div>
+                <div className='right'>
+                    <img alt='print' src={print} />
+                    <p>PRINT</p>
+                </div>
+                <img alt='pizza' src={pizza} />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="ingredients">
+                    {ingList}
+                </div>
+            </div>
+        )
+    }
+}
